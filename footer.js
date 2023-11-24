3d049884c546922f8091d4a5ee4f0f4695692e50
djinn0 <dsr.1991@gmail.com>
2023-11-24 10:01:26 -0700

    Update footer.js

--- footer.js
+++ footer.js
@@ -14,7 +14,7 @@ function generateFooter()
     let d = new Date();
     let n = d.getFullYear(); /* Adds current year to footer */
     let f1 = `<li class="footer-element"><a href="https://www.linkedin.com/in/david-redd">&copy David Redd ${n}</a></li>`
-    let f2 = '<li class="footer-element"><a href="aboutme.html">About</a></li>'
+    let f2 = '<li class="footer-element"><a href="about.html">About</a></li>'
     let f3 = '<li class="footer-element"><a href="aboutme.html">Contact</a></li>'
     x.insertAdjacentHTML('beforeend','<ul class="footer-bar" id="fb">' + f1 + f2 + f3 + '</ul>');
 
