fc2987a8e75aa17e9075e6bd85fafcb57bbc1bfb
djinn0 <dsr.1991@gmail.com>
2023-12-30 19:12:26 -0700

    Update NavBarPractice.js

--- NavBarPractice.js
+++ NavBarPractice.js
@@ -7,17 +7,16 @@ function generateMobileNavBar(){
   var e1 = "<a class='test' href='list2.html'>Reviews</a>”;
   var e2 = "<a class='test' href='ArticleList2.html'>Articles</a>";
   var e3 = "<a class='test' href='gridPractice2.html'>About</a>";
-  b.insertAdjacentHTML('afterbegin',"<details><summary>Home</summary>" + e1 + e2 + e3 + "</details>");
+  b.insertAdjacentHTML('afterbegin', "<details><summary>Home</summary><div>" + e1 + e2 + e3 + "</div></details>");
 }
 
 function generateStyling(){
   var s = document.getElementsByTagName("style")[0]
-  var sm1 = 'details > summary {margin: auto; width: 100%; text-align: center;  background-color: #333;  padding: 0.5rem 1rem; }';
-  var sm2 = 'summary {list-style: none; display: flex; justify-content: space-between;}';
-  var sm3 = 'summary:after{content: "\2630";}';
-  var sm4 = 'details[open] summary:after{ content: "\00D7";}';
-  s.insertAdjacentHTML('beforeend', sm1 + sm2 + sm3 + sm4);;
+  var sm1 = "details > summary { margin: auto; width: 100%; text-align: center; color: white; font-size: 4vmin; background-color: #333; padding: 0.5rem 1rem;}";
+  var sm2 = "details > div { margin: auto; width: 100%; background-color: #333; margin-top: 0; padding: 1rem; color: white;}";
+  var sm3 = "summary {list-style: none; display: flex; justify-content: space-between; }";
+  var sm4 = "summary:after{ content: "\2630";}";
+  var sm5 = "details[open] summary:after{ content: "\00D7";}"
+  var sm6 = ".test{ font-size: 4vmin; text-decoration: none; color: white; margin-left: 2rem; display: block;}"
+  s.insertAdjacentHTML('beforeend', sm1 + sm2 + sm3 + sm4 + sm5 + sm6 );
 }
-
-
-
