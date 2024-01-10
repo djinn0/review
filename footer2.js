1a4a51a6c51e37d2659bc34f06a0593efb180930
djinn0 <dsr.1991@gmail.com>
2024-01-09 20:04:36 -0700

    Update footer2.js

--- footer2.js
+++ footer2.js
@@ -6,11 +6,18 @@ window.addEventListener('load', (event) => {
 
 function generateFooter()
 {
+  /* generate footer bar content */
   var x = document.getElementsByTagName("body")[0];
+  let anchor = '<div id="anchor">' + "\u2693" + '</div>';
   let d = new Date();
   let n = d.getFullYear(); /* Adds current year to footer */
   let e1 = '<a href="contact.html">Contact</a>';
   let e2 = '<a href="disclosure.html">SexToyAhoy &copy ${n}</a>';
   let e3 = '<a href="disclosure.html">Disclosure</a>';
   let element = '<div id="element">' + e1 + e2 + e3 + '</div>';
+  x.insertAdjacentHTML('beforeend', '<div class="footer">' + anchor + element + '</div>');
+
+  /* add styling */
+  var s = document.getElementsByTagName("style")[0];
+  
 }
