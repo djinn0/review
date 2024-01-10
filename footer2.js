648eaf404298711541ddbfab9496c91f04a1731d
djinn0 <dsr.1991@gmail.com>
2024-01-10 09:53:14 -0700

    Update footer2.js

--- footer2.js
+++ footer2.js
@@ -20,11 +20,14 @@ function generateFooter()
   /* styling */
   var s = document.getElementsByTagName("style")[0];
   var sf1 = 'div.footer {position: fixed; bottom: 0; width: 100%; background-color: #333; }';
-  var sf2 = '#anchor{color: white; font-size: 6vmin; text-align: center;}';
+  var sf2 = '#anchor{color: white; font-size: 1.75em; text-align: center;}';
   var sf3 = '#element { padding: 8px 20px; display: flex; justify-content: space-between; }';
-  var sf4 = '#element > a { color: white; text-decoration: none; font-size: 4vmin; display: inline-block; }';
+  var sf4 = '#element > a { color: white; text-decoration: none; font-size: 1.5em; display: inline-block; }';
+  /* styling mobile */
+  var sm1 = '#anchor{color: white; font-size: 6vmin; text-align: center;}';
+  var sm2 = '#element > a { color: white; text-decoration: none; font-size: 4vmin; display: inline-block; }';
   s.insertAdjacentHTML('beforeend', sf1 + sf2 + sf3 + sf4 );
-  
+  s.insertAdjacentHTML('beforeend', '@media only screen and (max-width: 1000px){' + sm1 + sm2 + '}');
 }
     
  function test() {
