4dcafaca0fbdb71a82d644692761912fac9556d7
djinn0 <dsr.1991@gmail.com>
2024-01-11 16:46:02 -0700

    Update footer3.js

--- footer3.js
+++ footer3.js
@@ -11,11 +11,12 @@ function generateFooter()
   const html = document.documentElement;
   let contentHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
   let screenHeight = screen.height; 
- if (screenHeight > contentHeight ) {
+  /*
+    if (screenHeight > contentHeight ) {
      var c1 = 'screen';
   } else {
      var c1 = 'content';
   }
-  
+  */
   x.insertAdjacentHTML('beforeend', '<h1>' + contentHeight + ' ' + screenHeight '</h1>');
 }
