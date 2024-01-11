bb7a13d87f3ec09f7615b753779a1b05b45cd1e0
djinn0 <dsr.1991@gmail.com>
2024-01-11 16:54:03 -0700

    Update footer3.js

--- footer3.js
+++ footer3.js
@@ -7,16 +7,15 @@ function generateFooter()
     var x = document.getElementsByTagName("body")[0];
     
   /* content and screen heights */
-  const body = document.body;
+  /*const body = document.body;
   const html = document.documentElement;
   let contentHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
   let screenHeight = screen.height; 
-  /*
     if (screenHeight > contentHeight ) {
      var c1 = 'screen';
   } else {
      var c1 = 'content';
   }
   */
-  x.insertAdjacentHTML('beforeend', '<h1>' + contentHeight + ' ' + screenHeight '</h1>');
+  x.insertAdjacentHTML('beforeend', '<h1>Hello World</h1>');
 }
