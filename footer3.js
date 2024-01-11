d263b628764e1475a8e4da58bfd624941e620fef
djinn0 <dsr.1991@gmail.com>
2024-01-11 16:08:05 -0700

    Create footer3.js

---
+++ footer3.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', (event) => { 
+    generateFooter();
+});
+
+function generateFooter()
+{
+    var x = document.getElementsByTagName("body")[0];
+    
+    x.insertAdjacentHTML('beforeend', '<div class="footer">test 1</div>');
+
+  var s = document.getElementsByTagName("style")[0];
+  /* content and screen heights */
+  const body = document.body;
+  const html = document.documentElement;
+  var contentHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+  var screenHeight = screen.height; 
+  if (screenHeight > contentHeight ) {
+    var sf1 = 'div.footer {position: fixed; bottom: 0; z-index: 11; width: 100%; background-color: #333; }';
+  } else {
+    var sf1 = 'div.footer {position: static; z-index: 11; width: 100%; background-color: #333; }';
+  }
+  s.insertAdjacentHTML('beforeend', sf1);
+}
