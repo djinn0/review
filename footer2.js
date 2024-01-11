aa292887b511ad0b316744c93e552feb7849630c
djinn0 <dsr.1991@gmail.com>
2024-01-11 08:00:54 -0700

    Update footer2.js

--- footer2.js
+++ footer2.js
@@ -12,7 +12,7 @@ function generateFooter()
   let n = d.getFullYear(); /* Adds current year to footer */
   let e1 = '<a href="contact.html">Contact</a>';
   let e2 = '<a href="disclosure.html">Ahoy &copy' + n + '</a>';
-  let e3 = '<a href="disclosure.html">Disclosure 8</a>';
+  let e3 = '<a href="disclosure.html">Disclosure 9</a>';
   let element = '<div id="element">' + e1 + e2 + e3 + '</div>';
   let anchor = '<div id="anchor">' + "\u2693" + '</div>';
   x.insertAdjacentHTML('beforeend', '<div class="footer">' + element + anchor + '</div>');
@@ -27,7 +27,7 @@ function generateFooter()
   if (screenHeight > contentHeight ) {
     var sf1 = 'div.footer {position: fixed; bottom: 0; z-index: 11; width: 100%; background-color: #333; }';
   } else {
-    var sf1 = 'div.footer {position: static; z-index 11; width: 100%; background-color: #333; }';
+    var sf1 = 'div.footer {position: static; z-index: 11; width: 100%; background-color: #333; }';
   }
   var sf2 = '#anchor{color: white; font-size: 1.75em; text-align: center;}';
   var sf3 = '#element { padding: 8px 40px; display: flex; justify-content: space-between; }';
