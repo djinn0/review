c80f3f61cae91aef8f450795fb953e4716d53095
djinn0 <dsr.1991@gmail.com>
2024-01-11 06:57:55 -0700

    Update footer2.js

--- footer2.js
+++ footer2.js
@@ -8,14 +8,14 @@ function generateFooter()
 {
   /* generate footer bar content */
   var x = document.getElementsByTagName("body")[0];
-  let anchor = '<div id="anchor">' + "\u2693" + '</div>';
   let d = new Date();
   let n = d.getFullYear(); /* Adds current year to footer */
   let e1 = '<a href="contact.html">Contact</a>';
   let e2 = '<a href="disclosure.html">Ahoy &copy' + n + '</a>';
-  let e3 = '<a href="disclosure.html">Disclosure 7</a>';
+  let e3 = '<a href="disclosure.html">Disclosure 8</a>';
   let element = '<div id="element">' + e1 + e2 + e3 + '</div>';
-  x.insertAdjacentHTML('beforeend', '<div class="footer">' + anchor + element + '</div>');
+  let anchor = '<div id="anchor">' + "\u2693" + '</div>';
+  x.insertAdjacentHTML('beforeend', '<div class="footer">' + element + anchor + '</div>');
   
   /* styling */
   var s = document.getElementsByTagName("style")[0];
@@ -30,7 +30,7 @@ function generateFooter()
     var sf1 = 'div.footer {width: 100%; background-color: #333; }';
   }
   var sf2 = '#anchor{color: white; font-size: 1.75em; text-align: center;}';
-  var sf3 = '#element { padding: 8px 20px; display: flex; justify-content: space-between; }';
+  var sf3 = '#element { padding: 8px 40px; display: flex; justify-content: space-between; }';
   var sf4 = '#element > a { color: white; text-decoration: none; font-size: 1.5em; display: inline-block; }';
   /* styling mobile */
   var sm1 = '#anchor{color: white; font-size: 6vmin; text-align: center;}';
