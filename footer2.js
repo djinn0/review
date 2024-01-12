33bf5017b900c6e67605441e0f746a6fac4be783
djinn0 <dsr.1991@gmail.com>
2024-01-12 07:34:46 -0700

    Update footer2.js

--- footer2.js
+++ footer2.js
@@ -12,17 +12,19 @@ function generateFooter()
   let n = d.getFullYear(); /* Adds current year to footer */
   let e1 = '<a href="contact.html">Contact</a>';
   let e2 = '<a href="disclosure.html">Ahoy &copy' + n + '</a>';
-  let e3 = '<a href="disclosure.html">Disclosure 9</a>';
+  let e3 = '<a href="disclosure.html">Disclosure 10</a>';
   let element = '<div id="element">' + e1 + e2 + e3 + '</div>';
   let anchor = '<div id="anchor">' + "\u2693" + '</div>';
   x.insertAdjacentHTML('beforeend', '<div class="footer">' + element + anchor + '</div>');
   
   /* styling */
   var s = document.getElementsByTagName("style")[0];
-  /* content and screen heights */
+  /* 
   const body = document.body;
   const html = document.documentElement;
   var contentHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+  */
+  var contentHeight = Math.max(document.body.scrollHeight, document.documentElement.offsetHeight);
   var screenHeight = screen.height; 
   if (screenHeight > contentHeight ) {
     var sf1 = 'div.footer {position: fixed; bottom: 0; z-index: 11; width: 100%; background-color: #333; }';
