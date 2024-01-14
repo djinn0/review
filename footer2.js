f2dfce0d7a859c9703bc3acc29ec93ce85d23d8a
djinn0 <dsr.1991@gmail.com>
2024-01-14 16:16:04 -0700

    Update footer2.js

--- footer2.js
+++ footer2.js
@@ -41,22 +41,4 @@ function generateFooter()
   s.insertAdjacentHTML('beforeend', '@media only screen and (max-width: 1000px){' + sm1 + sm2 + '}');
 }
     
- function test() {
-
-  /* styling */
-  var s = document.getElementsByTagName("style")[0];
-  /* get content height */
-  const body = document.body;
-  const html = document.documentElement;
-  var contentHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
-  /* get screen height */
-  var screenHeight = screen.height; 
-  /* place footer at bottom of content or bottom of screen */
-  if (contentHeight > screenHeight){ let sf1 = 'div.footer { width: 100%; background-color: #333; }';}
-  else{ let sf1 = 'div.footer {position: fixed; bottom: 0; width: 100%; background-color: #333; }';}
-  /* additional Styling */  
-  var sf2 = '#anchor{color: white; font-size: 6vmin; text-align: center;}';
-  var sf3 = '#element { padding: 8px 20px; display: flex; justify-content: space-between; }';
-  var sf4 = '#element > a { color: white; text-decoration: none; font-size: 4vmin; display: inline-block; }';
-  s.insertAdjacentHTML('beforeend', sf1 + sf2 + sf3 + sf4);
-}
+ 
